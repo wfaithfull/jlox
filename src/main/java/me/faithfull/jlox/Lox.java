dceadf1df54e9cdcdfa49ff04ae482d53aa308bd
Will Faithfull <will@sourcepulp.com>
2017-03-24 14:43:34 +0000

    Initial commit

---
+++ src/main/java/me/faithfull/jlox/Lox.java
@@ -0,0 +1,64 @@
+package me.faithfull.jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Will Faithfull
+ */
+public class Lox {
+
+    private static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Usage: jlox <script>");
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String file) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(file));
+        run(new String(bytes, Charset.defaultCharset()));
+        if(hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while(true) {
+            System.out.print("> ");
+            run(reader.readLine());
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Lexer lexer = new Lexer(source);
+        List<Token> tokens = lexer.lex();
+
+        for(Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+}
